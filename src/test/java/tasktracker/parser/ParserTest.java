0ed8dc5e69a6f8bd056c72081519899c9c85ef20
ykteo000 <177651145+ykteo000@users.noreply.github.com>
2026-08-24 23:35:54 +0800

    Add A-JUnit to TaskTracker

---
+++ src/test/java/tasktracker/parser/ParserTest.java
@@ -0,0 +1,25 @@
+package tasktracker.parser;
+
+import org.junit.jupiter.api.Test;
+import tasktracker.task.Deadline;
+import tasktracker.task.ToDo;
+import tasktracker.exception.TaskTrackerException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParserTest {
+
+	@Test
+	public void parseToDo_validInput_returnsToDo() throws TaskTrackerException {
+		ToDo todo = Parser.parseToDo("read book");
+		assertEquals("[T][ ] read book", todo.toString());
+	}
+
+	@Test
+	public void parseToDo_emptyDescription_exceptionThrown() {
+		assertThrows(TaskTrackerException.class, () -> {
+			Parser.parseToDo("   ");
+		});
+	}
+}
